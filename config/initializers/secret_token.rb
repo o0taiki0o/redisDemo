6fc2a05183929321ab6f71c22dc09c1e69c911f7
Ta Duy Anh <black.cat.3007@gmail.com>
2012-06-18 14:23:24 +0700

    First Upload Redis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisDemo::Application.config.secret_token = '4864cdca112d56a53ce15ec4499af4d97cfc0d517511f3e5557ef1fb13332fadba33b1e81f8d32ef5e009e1db754ac0c7d84b2e608b3c7698b704c79b64beb9c'
